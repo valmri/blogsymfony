af72b76a23b56cd471ba787c18851e2a02756ccf
marmie <valentin.marmie@outlook.com>
2022-02-26 16:05:24 +0100

    Ajout d'article

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +11,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepo): Response
     {
+
+        $articles = $articleRepo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
     #[Route('/', name: 'home')]
     public function home() {
-        return $this->render('blog/home.html.twig');
+        return $this->render('blog/home.html.twig', [
+            'Prenom' => "Valentin"
+        ]);
+    }
+
+    #[Route('/blog/{id}', name: 'blog_show')]
+    public function show(ArticleRepository $articleRepo, int $id){
+
+        $monArticle = $articleRepo->find($id);
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $monArticle
+        ]);
     }
 }
